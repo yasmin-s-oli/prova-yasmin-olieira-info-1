cae83ea4d417e82c1b3351c0087629e5c1ac2a11
yasmin-s-oli <yasmin.oliveira1@aluno.ifsp.edu.br>
2025-04-11 15:04:43 -0300

    Add files via upload

---
+++ ex4.js
@@ -0,0 +1,42 @@
+const banco = []
+const devolucoes1 = {
+    autor:"Collen hover",
+    titulo:"As mil partes do meu coração",
+    genero:"romance",}
+    
+
+    
+console.log (devolucoes1)
+
+const devolucoes2 = {
+    autor:"Collen hover",
+    titulo:"È assim que acaba",
+    genero:"Romance"
+}
+
+console.log (devolucoes2)
+
+const devolucoes3 = {
+    autor:"Collen hover",
+    titulo:"È assim que começa",
+    genero:"Romance"
+}
+
+console.log (devolucoes3)
+
+delete devolucoes1.autor;
+delete devolucoes1.titulo;
+delete devolucoes1.genero;
+console.log(devolucoes1)
+
+delete devolucoes2.autor;
+delete devolucoes2.titulo;
+delete devolucoes2.genero;
+console.log(devolucoes2)
+
+delete devolucoes3.autor;
+delete devolucoes3.titulo;
+delete devolucoes3.genero;
+console.log(devolucoes3)
+
+
---
+++ prova.js
@@ -0,0 +1,10 @@
+const banco = [ ]
+console.log (banco)
+const bancoFila = ['yasmin', 'samuel', 'mirela']
+console.log (bancoFila)
+bancoFila.pop ('yasmin', 'samuel', 'mirela')
+console.log(bancoFila)
+bancoFila.pop ("yasmin", "samuel")
+console.log(bancoFila)
+bancoFila.pop ("yasmin")
+console.log(bancoFila)
